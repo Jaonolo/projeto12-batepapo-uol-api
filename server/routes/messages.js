fa3d74c26a22ed2a1abde000b33749ea5d9cdbb8
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-05-02 06:08:37 -0300

    feat : a lot of changes. delete and put bonuses for messages route and first implementation of participants autoRemove

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,4 +1,5 @@
 import db from "../db.js"
+import { ObjectId } from "mongodb"
 
 const get = async (req, res) => {
     const { limit } = req.query
@@ -26,4 +27,58 @@ const post = (req, res) => {
     }).then(() => res.sendStatus(201))
 }
 
-export default { get: get, post: post }
+const remove = async (req, res) => {
+    const { user } = req.headers
+    const { messageId } = req.params
+
+    const mensagem = await db.collection("messages").findOne({
+        _id: ObjectId(messageId)
+    })
+
+    if(!mensagem) {
+        console.log(mensagem, messageId)
+        res.sendStatus(404)
+        return
+    }
+
+    if(mensagem.from !== user) {
+        res.sendStatus(401)
+        return
+    }
+
+    const retorno = await db.collection("messages").deleteOne(mensagem)
+    res.send(retorno)
+}
+
+const put = async (req, res) => {
+    const { to, text, type } = req.body
+    const { user } = req.headers
+    // validation
+
+    const { messageId } = req.params
+
+    const mensagem = await db.collection("messages").findOne({
+        _id: ObjectId(messageId)
+    })
+
+    if(!mensagem) {
+        console.log(mensagem, messageId)
+        res.sendStatus(404)
+        return
+    }
+
+    if(mensagem.from !== user) {
+        res.sendStatus(401)
+        return
+    } 
+
+    await db.collection("messages").updateOne(
+        mensagem, 
+        { 
+            $set: req.body
+        }
+    )
+    res.sendStatus(200)
+}
+
+export default { get: get, post: post, delete: remove, put: put }
